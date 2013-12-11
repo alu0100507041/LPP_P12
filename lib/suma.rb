7ff2614bc879ce98f0376259b1ae06fdd4d77962
Salome Gonzalez <alu0100507041@ull.edu.es>
2013-12-11 10:54:41 +0000

    Añadiendo suma.rb

---
+++ lib/suma.rb
@@ -0,0 +1,13 @@
+#Tipo de operacion
+operacion "suma"
+
+#Muestra por consola
+opcion "consola"
+
+#Matrices densas
+opcion "densa"
+operando [1,2],[2,3]
+
+#Matrices dispersas
+opcion "dispersa"
+operando [1,0],[2,0.0]
